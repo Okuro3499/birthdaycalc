783ea83ef96c2164b28fc540bc83f935175f02d5
Gideon Okuro <gideonollonde@gmail.com>
2020-05-11 22:09:09 +0300

    edit functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 function getAkanName(){
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  var maleAkannames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-  var femaleAkannames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-  var myBirthday = document.getElementById("myBirthDate").value;
-  var myGender = document.getElementById("gender").value;
-  var dateOfBirth = new Date(myBirthdate);
-  var dayOfTheWeek = dateOfBirth.getDay();
+  var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+  var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+  var myBirthday = document.getElementById('birth').value;
+  var myGender = document.getElementById('genders').value;
+  var d = new Date(myBirthday);
   var CC, MM, YY, DD;
   if (!!d.valueOf()) {
     YY = d.getFullyear();
@@ -16,12 +15,12 @@ function getAkanName(){
   var daycalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
   dayCalculate = dayCalculate + 1;
   console.log(daycalculate);
-  if (document.getElementById('maleAkannames').checked) {
-    myGender = document.getElementById('maleAkannames').value;
+  if (document.getElementById('male').checked) {
+    myGender = document.getElementById('male').value;
     alert("You were born on " + dayOfTheWeek[dayCalculate] + " Your name is " + maleAkannames[dayCalculate]);
   }
-  if (document.getElementById('femaleAkannames').checked) {
-    myGender = document.getElementById('femaleAkannames').value;
+  if (document.getElementById('female').checked) {
+    myGender = document.getElementById('female').value;
     alert(" You were born on " + dayOfTheWeek[dayCalculate] + " Your name is " + femaleAkannames[dayCalculate]);
   }
   document.getElementById('message').innerHTML = dayOfTheWeek[dayCalculate];
