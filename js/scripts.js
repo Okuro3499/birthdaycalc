25db1adf924d191c92c837a73d7247fc67e87a13
Gideon Okuro <gideonollonde@gmail.com>
2020-05-13 13:00:51 +0300

    edit script to split user input into month, day and year

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-function getAkanName(){
+function getAkanname(){
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
   var myBirthday = document.getElementById('birth').value;
+  myBirthday= myBirthday.split("/");
+  var day =parseInt(myBirthday[1]);
+  var month= parseInt(myBirthday[0]);
+  var year= parseInt(myBirthday[2]);
+  var century = parseInt(myBirthday[2].substring(0,2));
+  console.log(day, month, year, century );
   var myGender = document.getElementById('genders').value;
-  var d = new Date(myBirthday);
-  var CC, MM, YY, DD;
-  if (!!d.valueOf()) {
-    YY = d.getFullyear();
-    MM = d.getMonth();
-    CC = d.getDate();
-    DD = d.getDay();
-  } else {/*Invalid date */}
-  var daycalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-  dayCalculate = dayCalculate + 1;
-  console.log(daycalculate);
+
+  var dayCalculate = Math.floor(parseInt(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
+  dayCalculate= dayCalculate - 2;
+  console.log(dayCalculate)
   if (document.getElementById('male').checked) {
     myGender = document.getElementById('male').value;
-    alert("You were born on " + dayOfTheWeek[dayCalculate] + " Your name is " + maleAkannames[dayCalculate]);
+    alert("You were born on " + days[dayCalculate] + " Your name is " + male[dayCalculate]);
   }
   if (document.getElementById('female').checked) {
     myGender = document.getElementById('female').value;
-    alert(" You were born on " + dayOfTheWeek[dayCalculate] + " Your name is " + femaleAkannames[dayCalculate]);
+    alert(" You were born on " + days[dayCalculate] + " Your name is " + female[dayCalculate]);
   }
-  document.getElementById('message').innerHTML = dayOfTheWeek[dayCalculate];
+  document.getElementById('message').innerHTML = days[dayCalculate];
 }
